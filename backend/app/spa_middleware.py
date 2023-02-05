16bab42b86786db484eeea720af373199fed2d3f
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2023-02-05 14:26:15 +0530

    Fix static file serving logic

--- backend/app/spa_middleware.py
+++ backend/app/spa_middleware.py
@@ -4,10 +4,14 @@ from aiohttp import web
 def with_html(body: bytes):
     @web.middleware
     async def spa_middleware(request: web.Request, handler):
-        return (
-            await handler(request)
-            if request.path.startswith("/x/")
-            else web.Response(body=body, content_type="text/html")
-        )
+        if request.path == "/":
+            return web.Response(body=body, content_type="text/html")
+
+        try:
+            return await handler(request)
+        except web.HTTPNotFound:
+            if request.path.startswith("/x/"):
+                raise
+            return web.Response(body=body, content_type="text/html")
 
     return spa_middleware
