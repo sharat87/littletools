b44a4f998ebd0692d5458158fc78e7bbc32ada65
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2023-02-05 13:36:16 +0530

    Fix connection freeze

--- backend/app/spa_middleware.py
+++ backend/app/spa_middleware.py
@@ -2,10 +2,12 @@ from aiohttp import web
 
 
 def with_html(body: bytes):
-    body_response = web.Response(body=body, content_type="text/html")
-
     @web.middleware
     async def spa_middleware(request: web.Request, handler):
-        return await handler(request) if request.path.startswith("/x/") else body_response
+        return (
+            await handler(request)
+            if request.path.startswith("/x/")
+            else web.Response(body=body, content_type="text/html")
+        )
 
     return spa_middleware
