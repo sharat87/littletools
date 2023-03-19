dd26c0bee9b71abf81bd1936dedcdfff74d93e05
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2023-03-19 10:07:53 +0530

    Fix CI

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,11 +3,6 @@ import pytest
 
 
 @pytest.fixture
-def anyio_backend():
-    return "asyncio"
-
-
-@pytest.fixture
 async def client_session() -> aiohttp.ClientSession:
     async with aiohttp.ClientSession("http://localhost:3061") as session:
         yield session
