4bf65500f22bb414772a3cb44d904093ef484abc
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2024-04-22 23:12:06 +0530

    Fix SVG tool

--- frontend/src/tools/svg-preview.ts
+++ frontend/src/tools/svg-preview.ts
@@ -4,6 +4,14 @@ import { LanguageSupport } from "@codemirror/language"
 import { customJSONLang } from "./json"
 import { CodeMirror, ToolView } from "../components"
 
+const PREFIX: string = `<!doctype html>
+<style>
+html, body {
+	background: none transparent;
+}
+</style>
+`
+
 const initialSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="128" height="128" fill="none"
 \t\tstroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="3">
 \t<path d="M 20 40 Q 40 24 20 14 v 10 m 0 -10 h 10"/>
@@ -21,7 +29,7 @@ export default class extends ToolView {
 			m("div", "This is a WIP. Content is not saved. Put some SVG in the editor and see it below live. Useful to edit/author small SVG icons."),
 			m(CodeMirror, {
 				doc: this.content,
-				onDocChanged(updape: ViewUpdate) {
+				onDocChanged: (updape: ViewUpdate) => {
 					this.content = updape.state.doc.toString()
 					m.redraw()
 				},
@@ -30,8 +38,10 @@ export default class extends ToolView {
 					new LanguageSupport(customJSONLang),
 				],
 			}),
-			this.editor != null && m(".flex-1.hstack.justify-content-around.svg-preview", m.trust(this.content)),
-			m("style", ".svg-preview svg { border: 1px solid #ccc; }"),
+			m("iframe.flex-1.rounded", {
+				allowTransparency: "true",
+				srcdoc: PREFIX + this.content,
+			}),
 		]
 	}
 }
